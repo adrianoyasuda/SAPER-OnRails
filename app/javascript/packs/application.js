22d521dac91b37c7ec517237f91ba0f1205c9ffb
Igor Miranda <Nigtow@hotmail.com>
2019-09-19 17:29:24 -0300

    Bootstrap and Jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("./test")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -15,3 +16,9 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import 'bootstrap'
+
+document.addEventListener("turbolinks:load", () => {
+    $('[data-toggle="tooltip"]').tooltip()
+})
