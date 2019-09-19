db1f746b4bcf36d0fc71fbbe8bf1f41bcfcbab3a
Adriano Yasuda <adriano__kira@hotmail.com>
2019-09-19 10:26:38 -0300

    Index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :questions
   resources :users
+
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
