a2035e2f151f07a3b9b698d58ff8d9541f1ea394
Adriano Yasuda <adriano__kira@hotmail.com>
2019-10-17 12:39:36 -0300

    Applications view

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,11 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require("./test")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
+require("./test");
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -19,4 +19,6 @@ require("./test")
 
 import 'bootstrap';
 import '../stylesheets/application';
+
+
 
