16c64a636eba6d456ac242f6ed60e76f603a2660
Adriano Cesar Veloso Cruz Yasuda <adriano__kira@hotmail.com>
2019-11-21 15:12:10 -0200

    ipload+-

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
 require("./test");
+require("./imgur");
+require("./upload");
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
